ccc4ca198710a7b6c2e0cb1fe254692a4f1ac1e0
Gabriel Senan <gabrielsnnh50@gmail.com>
2024-05-16 20:01:53 -0300

    whats

--- src/js/script.js
+++ src/js/script.js
@@ -3,4 +3,12 @@ $(document).ready(function() {
         $('#mobile-menu').toggleClass('active');
         $('#mobile-btn').find('i').toggleClass('fa-x');
     });
+
+    const sections = $('section');
+    const navItens = $('.nav-list');
+
+    $(window).on('scroll', function () {
+        const header = $ ('header');
+        const scrollPosition
+    })
 });
